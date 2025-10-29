c457560b7e4cc9ac16258375c0cf648cbfbb5c52
Jean Marcos <jeansouza@fineasytech.com>
2025-10-29 08:55:51 -0300

    feat: add build flow

---
+++ frontend/electron/main.cjs
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 640,
+    webPreferences: {
+      sandbox: false,
+    },
+  });
+
+  const devServerUrl = process.env.VITE_DEV_SERVER_URL;
+
+  if (!app.isPackaged && devServerUrl) {
+    win.loadURL(devServerUrl);
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    const indexHtml = path.join(__dirname, '..', 'dist', 'index.html');
+    win.loadFile(indexHtml);
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
